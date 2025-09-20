c92330a9a995723bc73b9a1b61b7c42a50c0626a
Arun-Chandrasekaran928 <chandrasekarana@student.rcdsb.on.ca>
2025-09-20 15:24:48 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Big Website</h1>
+      <p>Welcome to your huge app!</p>
+    </div>
+  );
+}
+
+export default App;
